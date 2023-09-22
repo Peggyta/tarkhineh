707b6df0c71563420d48585e419427ab96107e74
Peggyta <peggyta9096@gmail.com>
2023-09-22 18:23:47 +0330

    creating a Card component for Branches

--- src/components/module/contactUs/BranchCard.js
+++ src/components/module/contactUs/BranchCard.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Image from 'next/image';
 
 const BranchCard = (props) => {
+    const {title, phone, address, work, slug} = props;
     return (
-        <div>
-            
+        <div className='flex items-center justify-start gap-20 border border-neutral'>
+            <div>
+                <Image src={`/images/${slug}.jpg`} alt='branches' width={600} height={280} />
+            </div>
+            <div className='text-neutral flex flex-col gap-2 text-lg'>
+                <h3 className='text-center font-semibold text-xl text-raven'>{title}</h3>
+                <p>آدرس: {address}</p>
+                <p>شماره تماس: {phone}</p>
+                <p>ساعت کاری: {work}</p>
+            </div>
         </div>
     );
 };
