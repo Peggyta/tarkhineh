26868e7dd406c6d11f4990a4490401bc446a1118
Peggyta <peggyta9096@gmail.com>
2023-09-22 15:39:04 +0330

    fetching data from database and passing it as a props

--- src/app/branches/page.js
+++ src/app/branches/page.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import connectDB from '@/utils/connectDB';
+import TBranch from '@/models/TBranch';
 import Branches from '@/components/template/Branches';
 
-const BranchesPage = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+async function BranchesPage() {
+    await connectDB();
+    const branches = await TBranch.find();
+    if(!branches) return <h3>مشکلی پیش آمده است. لطفا دوباره امتحان کنید</h3>
+    return <Branches showBranches={JSON.parse(JSON.stringify(branches))}  />
 };
 
 export default BranchesPage;
