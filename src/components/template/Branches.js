191025fe13a572caadcf25e88672e2ec732ecf1c
Peggyta <peggyta9096@gmail.com>
2023-09-25 18:22:26 +0330

    lifting state up to create some forms

--- src/components/template/Branches.js
+++ src/components/template/Branches.js
@@ -1,9 +1,27 @@
-import React from 'react';
+'use client';
+import React, {useState} from 'react';
 import Features from '../module/branching/Features';
 import BenefitOfBranching from '../module/branching/BenefitOfBranching';
+import TextInput from '../module/branching/TextInput';
 import styles from '../../styles/Branches.module.css';
 
 const Branches = () => {
+    const[formData, setFormData] = useState({
+        title:'',
+        identity: '',
+        nationalId: '',
+        phone: '',
+        perfectTime: '',
+        state: '',
+        city: '',
+        region: '',
+        typeOfOwnership: '',
+        area: '',
+        ageOfBuilding: '',
+        amenities: '',
+        address: [],
+
+    })
     return (
         <div>
             <div className={styles.branchBanner}>
@@ -13,6 +31,29 @@ const Branches = () => {
             </div>
             <Features />
             <BenefitOfBranching />
+            <div className='flex flex-col items-center text-raven'>
+                <h3 className='font-bold text-2xl'>دریافت مشاوره</h3>
+                <div className='flex'>
+                    <TextInput
+                    title='نام و نام‌خانوادگی'
+                    name='identity'
+                    formData={formData}
+                    setFormData={setFormData} />
+
+                    <TextInput
+                    title='شماره تماس'
+                    name='phone'
+                    formData={formData}
+                    setFormData={setFormData} />
+
+                    <TextInput
+                    title='زمان ایده‌آل'
+                    name='perfectTime'
+                    formData={formData}
+                    setFormData={setFormData} />
+                </div>
+                <button className='bg-primary text-white'>درخواست مشاوره</button>
+            </div>
         </div>
     );
 };
