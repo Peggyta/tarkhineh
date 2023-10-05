a9e4a6a01035400b13bd1ed757acc54ffc2195bc
Peggyta <peggyta9096@gmail.com>
2023-10-05 18:34:27 +0330

    branch details page is now responsive

--- src/components/template/BranchDetails.js
+++ src/components/template/BranchDetails.js
@@ -8,21 +8,21 @@ import LocationIcon from '../icons/LocationIcon';
 const BranchDetails = ({data}) => {
     const{work, title, phone, slug, address}= data;
     return (
-        <div className='flex flex-col items-center'>
-            <h2 className='font-bold lg:text-2xl sm:text-lg text-raven text-sm pb-4'>{title}</h2>
+        <div className='flex flex-col items-center sm:px-10 px-4'>
+            <h2 className='font-bold lg:text-2xl text-lg text-raven pb-4'>{title}</h2>
             <div className='h-full relative '>
                 <Image src={`/images/${slug}.jpg`} alt='branches' width={800} height={336} />
-                <div className='flex justify-between border border-primary text-neutral text-xs w-5/6 
-                h-24 items-center px-4 mx-auto absolute z-10 -bottom-10 left-20 bg-white rounded-md'>
-                    <div className='flex flex-col items-center gap-2'>
+                <div className='flex md:flex-row flex-col justify-between border border-2 border-primary text-neutral text-xs md:w-5/6 w-full 
+                md:h-24 h-full md:pt-0 md:pb-0 pt-6 pb-10 items-center px-4 mx-auto md:absolute md:z-10 md:-bottom-10 md:left-20 bg-white rounded-md'>
+                    <div className='flex md:flex-col items-center gap-2 pb-4 md:pb-0'>
                         <Calling />
                         <p>{phone}</p>
                     </div>
-                    <div className='flex flex-col items-center gap-2'>
+                    <div className='flex md:flex-col items-center gap-2 pb-4 md:pb-0'>
                         <LocationIcon />
                         <p>{address}</p>
                     </div>
-                    <div className='flex flex-col items-center gap-2'>
+                    <div className='flex md:flex-col items-center gap-2 pb-4 md:pb-0'>
                         <Clock />
                         <p>{work}</p>
                     </div>
