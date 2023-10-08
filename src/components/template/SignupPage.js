3f19c906c7d93ea05adf525df2899324ad522f84
Peggyta <peggyta9096@gmail.com>
2023-10-08 15:43:00 +0330

    signup and signin forms created

--- src/components/template/SignupPage.js
+++ src/components/template/SignupPage.js
@@ -1,10 +1,54 @@
-import React from 'react';
+'use client';
+import React, {useState} from 'react';
+import Link from 'next/link';
+import Loader from '../icons/Loader';
 
 const SignupPage = () => {
+  const[loading, setLoading]= useState(false);
+  const[email, setEmail]= useState('');
+  const[password, setPassword]= useState('');
+  const[rePassword, setRePassword]= useState('');
+
     return (
-        <div>
-            
-        </div>
+      <div className='flex flex-col items-center justify-center pt-12 text-primary text-sm'>
+        <h4 className='text-2xl font-bold pb-5'>فرم ثبت نام</h4>
+        <form className='flex flex-col bg-white border border-2 border-primary rounded-md w-64 h-96 px-6 justify-evenly py-4'>
+          <div className='form-items'>
+            <label className='font-semibold'>ایمیل:</label>
+            <input
+              type='text'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className='forms-input'
+            />
+            </div>
+          <div className='form-items'>
+            <label className='font-semibold'>رمز عبور:</label>
+            <input
+              type='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className='forms-input'
+            />
+          </div>
+          <div className='form-items'>
+            <label className='font-semibold'>تکرار رمز عبور:</label>
+            <input
+              type='password'
+              value={rePassword}
+              onChange={(e) => setRePassword(e.target.value)}
+              className='forms-input'
+            />
+          </div> 
+          {loading ? <Loader /> : 
+          <button className='form-button'>ثبت‌نام</button>}
+        </form>
+        <p className='pt-2 text-neutral'>
+          حساب کاربری دارید؟
+          <Link className='font-bold text-primary border-b border-b-2 border-primary pb-0.5 
+          pr-0.5 hover:border-hovercolor' href="/signin">ورود</Link>
+        </p>
+    </div>
     );
 };
 
