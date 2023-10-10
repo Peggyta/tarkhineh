e3269571594b19f7f7cba6733115d389e249bcf7
Peggyta <peggyta9096@gmail.com>
2023-10-10 16:12:28 +0330

    metadata of user panel has been defined. limitation of accessibility to user panel for non-registered users

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../api/auth/[...nextauth]/route';
 import DashboardSidebar from '@/components/layout/DashboardSidebar';
+import { redirect } from 'next/navigation';
+import User from '@/models/User';
 
-const DashboardLayout = ({children}) => {
+export const metadata = {
+    title: 'پنل کاربری ترخینه',
+};
+
+async function DashboardLayout ({children}) {
+    const session = await getServerSession(authOptions);
+    if(!session) redirect('/signin');
+    const user = await User.findOne({email: session.user.email});
+    if(!user) return <h3 className='text-xl text-raven font-bold'>مشکلی پیش آمده است</h3>
     return (
-        <div>
-            <DashboardSidebar>
+        <div className='sm:px-10 px-4 pt-6'>
+            <DashboardSidebar role={user.role} email={user.email}>
                 {children}
             </DashboardSidebar>     
         </div>
