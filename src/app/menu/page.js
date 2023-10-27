1c7e52ea1930bd28f8043fe760352dcf286ac18c
Peggyta <peggyta9096@gmail.com>
2023-10-27 16:48:17 +0330

    fetching data from database and filtering them base on category

--- src/app/menu/page.js
+++ src/app/menu/page.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import connectDB from '@/utils/connectDB';
+import TProduct from '@/models/TProduct';
 import MainMenu from '@/components/template/MainMenu';
 
-const page = () => {
+async function MenuPage({searchParams}) {
+    
+    await connectDB();
+    let products = await TProduct.find();
+    if(searchParams.category) {
+        products = products.filter((i)=> i.category === searchParams.category)
+    };
     return (
         <div>
-            <MainMenu />
+            <MainMenu foodData={products} />
         </div>
     );
 };
 
-export default page;
+export default MenuPage;
