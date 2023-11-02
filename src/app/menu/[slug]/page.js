b72e10255497959df0196027de5119d3ab6462ef
Peggyta <peggyta9096@gmail.com>
2023-11-02 18:37:25 +0330

    fetch data by using its params

---
+++ src/app/menu/[slug]/page.js
@@ -0,0 +1,13 @@
+import connectDB from "@/utils/connectDB";
+import TProduct from "@/models/TProduct";
+import FoodDetail from "@/components/template/FoodDetail";
+
+async function DetailsPage({params:{slug}}) {
+    await connectDB();
+    const foodDetails = await TProduct.findOne({slug: slug});
+    console.log(foodDetails)
+    if(!foodDetails) return <h3>مشکلی پیش آمده است. لطفا دوباره امتحان کنید</h3>
+    return <FoodDetail details={JSON.parse(JSON.stringify(foodDetails))} />
+
+}
+export default DetailsPage;
