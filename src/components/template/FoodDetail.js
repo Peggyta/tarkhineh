d7b32362eee26a04d69be93cf6a2f1fdd75d0796
Peggyta <peggyta9096@gmail.com>
2023-11-05 16:24:32 +0330

    details page is responsive now

--- src/components/template/FoodDetail.js
+++ src/components/template/FoodDetail.js
@@ -2,7 +2,7 @@
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 import ProductRate from '../module/menu/ProductRate';
-
+import Back from '../icons/Back';
 
 
 export default function FoodDetail({details}) {
@@ -14,26 +14,27 @@ export default function FoodDetail({details}) {
   }
 
   return (
-    <>
-      <div className='flex flex-col items-center px-12 text-raven'>
-        <div className='flex justify-between w-full pt-4 pb-6'>
-          <p className='invisible'>fhc'aj</p>
-          <p className='text-xl font-semibold'>اطلاعات محصول</p>
-          <button className='bg-primary px-6 text-white rounded-md pb-1 pt-0.5 hover:bg-emerald-600 transition' 
-          onClick={backHandler}>بازگشت</button>
+    <div className='flex md:px-12 px-10 md:flex-row flex-col'>
+      <div className='flex flex-col items-center text-raven pb-10 md:w-[600px] w-full mx-auto'> 
+          <p className='sm:text-xl text-base font-semibold text-center pb-3'>اطلاعات محصول</p>
+        <div>
+          <Image src={`/images/${slug}.jpg`} alt='food' width={600} height={450} priority={true} />
         </div>
-      <div>
-          <Image src={`/images/${slug}.jpg`} alt='food' width={808} height={441} />
+        <div className='flex justify-between w-full pt-2'>
+          <p>{title}</p>
+          <ProductRate rating={rating} />
+        </div>
+        <div className='flex justify-start w-full'>
+          <p className='text-sm'>{ingredient}</p>
+        </div>   
       </div>
-      <div className='flex justify-between w-[808px] pt-2'>
-        <p>{title}</p>
-        <ProductRate rating={rating} />
+      <div className='flex justify-end md:block'>
+        <button className='bg-primary sm:pr-6 sm:pl-1 pr-4 text-white rounded-md sm:pb-1 sm:pt-0.5 
+        hover:bg-emerald-600 transition flex items-center sm:text-base text-sm pt-1 pb-2 gap-2' 
+          onClick={backHandler}>بازگشت
+          <Back />
+        </button>
       </div>
-      <div className='flex justify-start w-[808px]'>
-        <p className='text-sm'>{ingredient}</p>
-      </div>   
-      </div>
-                  
-    </>
+    </div>          
   )
 }
