2950b6202f0c921958ec8fd68e8777e7dacbf891
Peggyta <peggyta9096@gmail.com>
2023-11-11 16:08:55 +0330

    a component for searching products created

--- src/components/module/menu/SearchFilter.js
+++ src/components/module/menu/SearchFilter.js
@@ -1,25 +1,37 @@
-import React from 'react';
-import Link from 'next/link';
+'use client';
+import React, { useEffect, useState } from 'react';
 import SearchIcon from '@/components/icons/SearchIcon';
-import FilterSlider from './FilterSlider';
 
 const SearchFilter = () => {
+    const[showSearch, setShowSearch] = useState(false);
+    const[search, setSearch] = useState('');
+    const[searchTerm, setSearchTerm] = useState([]);
+    const[isLoading, setIsLoading] = useState(false);
+       
+
     return (
         <>
         <div className='flex lg:flex-row flex-col justify-between pt-4 md:pl-12 md:gap-6 w-full px-4' >
            <FilterSlider />
             <div className='flex items-center border border-bordercolor px-2 md:w-3/4 w-full rounded-md md:items-center hidden lg:flex'>
-                <input className='w-full pb-2 pt-1 text-sm font-medium' placeholder='جستجو' />
+                <input
+                    type='text'
+                    value={search}
+                    onChange={searchHandler} 
+                    className='w-full pb-2 pt-1 text-sm font-medium outline-none' placeholder='جستجو' />
                 <SearchIcon />
             </div>
         </div>
         <div className='px-4'>
         <div className='flex items-center border border-bordercolor px-2 sm:w-500 w-full mt-4 mx-auto rounded-md justify-center  lg:hidden'>
-                <input className='w-full pb-2 pt-1 text-sm font-medium' placeholder='جستجو' />
+                <input
+                    type='text'
+                    value={search}
+                    onChange={searchHandler} 
+                    className='w-full pb-2 pt-1 text-sm font-medium outline-none' placeholder='جستجو' />
                 <SearchIcon />
             </div>
-        </div>
-            
+        </div> 
     </>
     );
 };
