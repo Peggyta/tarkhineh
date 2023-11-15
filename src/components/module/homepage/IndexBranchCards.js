4fe2166617b0a46d1c7f95daa4ec9dc8c2651d3e
Peggyta <peggyta9096@gmail.com>
2023-11-15 14:05:52 +0330

    improvment in loading cards by using async function

--- src/components/module/homepage/IndexBranchCards.js
+++ src/components/module/homepage/IndexBranchCards.js
@@ -5,20 +5,22 @@ import IndexCard from './IndexCard';
 const IndexBranchCards = () => {
     const[data, setData] = useState([]);
     useEffect(()=> {
-        fetch('/api/branch')
-        .then(res => res.json())
-        .then(data => setData(data.data))
+        const fetchData = async() => {
+                await fetch('/api/branch')
+                .then(res => res.json())
+                .then(data => setData(data.data))
+        }
+        fetchData()
     },[])
     return (
         <div className='sm:px-10 px-4 text-raven mb-16'>
             <h3 className='font-bold md:text-2xl text-xl md:pb-6 pb-5 text-center'>ترخینه‌گردی</h3>
             <div className='grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-4'>
-                {!data.length ? ( <div>
-                            <p className='text-center font-bold text-lg'>در حال بارگزاری...</p>
-                        </div>) : (data.map((item)=>{
+                {!data.length && <p className='font-bold text-lg'>در حال بارگزاری...</p>}
+                {data.map((item)=>{
                     return <IndexCard key={item._id} {...item} />
                                              
-                }))} 
+                })}
             </div>
         </div>
     );
