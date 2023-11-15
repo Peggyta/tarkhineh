f372e02072d6ce4d7d7e5a619c0ff3c654f032c8
Peggyta <peggyta9096@gmail.com>
2023-11-15 15:00:31 +0330

    loader spinner changed

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,11 +1,10 @@
 import React from "react";
-import Image from "next/image";
-import loadImg from '../../public/images/loading.gif';
+import styles from '../styles/Loader.module.css';
 
 function Loading() {
     return(
-        <div className="bg-grass h-screen flex justify-center items-center">
-            <Image src={loadImg} alt="loading" width={350} height={350} />
+        <div className="h-screen flex justify-center items-center">
+            <span className={styles.loader}></span>
         </div>
     )
 }
