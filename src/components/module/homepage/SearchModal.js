1a9bd20e13cdbd51299b1025a6af7631a40e7a59
Peggyta <peggyta9096@gmail.com>
2023-11-29 14:45:44 +0330

    map on fetched data

--- src/components/module/homepage/SearchModal.js
+++ src/components/module/homepage/SearchModal.js
@@ -103,6 +103,52 @@ export default function SearchModal() {
           </div>
         </Dialog>
       </Transition>
+      <Transition appear show={isOpenResult} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeResult}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black/25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+              >
+                <Dialog.Panel className="w-3/4 h-[600px] transform max-w-6xl overflow-y-auto rounded-xl bg-white text-left align-middle shadow-xl transition-all">
+                  <div className='flex justify-between w-full bg-ash px-4 py-4'>
+                    <p className='invisible'>.....</p>
+                    <p className='text-raven text-xl font-semibold'> نتایج جستجو برای: 
+                    <span className='text-primary'> {title}</span></p>
+                    <button
+                      type="button"
+                      className='outline-none'
+                      onClick={closeResult}
+                    >
+                      <CancelDark />
+                    </button>
+                  </div>
+                  <div className="py-10 flex flex-col items-center">
+                    <div className='w-full place-items-center grid md:grid-cols-2 xl:grid-cols-3 gap-3'>
+                      {results.map((i)=> {
+                        return(
+                            <SearchResult key={i._id} data={i} />
+                          )})}  
+                    </div>    
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
     </>
   )
 }
